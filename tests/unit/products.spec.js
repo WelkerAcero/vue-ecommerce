d023d4feb5e99e099623cdbac9c2a8adc3ee14f2
WelkerAcero <universityestudiosca@gmail.com>
2024-03-04 18:52:26 -0500

    Adding bable.config.js settings

--- tests/unit/products.spec.js
+++ tests/unit/products.spec.js
@@ -1,14 +1,39 @@
 import { shallowMount } from "@vue/test-utils";
-import { mount } from "@vue/test-utils";
-import shoppingCartComponent from "@/components/shoppingCartComponent.vue";
+//import { mount } from "@vue/test-utils";
+import ShoppingCart from '@/components/shoppingCartComponent.vue';
 
-describe('shoppingCartComponent.vue', () => {
-    it('Render list of added products', () => {
-        const ITEMS = '';
-        const WRAPPER = shallowMount(shoppingCartComponent, {
-            props: { msg }
+describe('ShoppingCartComponent.vue', () => {
+    it('Renders correctly when isVisible is true', () => {
+        const wrapper = shallowMount(ShoppingCart, {
+            props: {
+                isVisible: true,
+                objAdded: [],
+            }
+        });
+        expect(wrapper.isVisible()).toBe(true);
+    });
+
+    it('Renders correctly when isVisible is false', () => {
+        const wrapper = shallowMount(ShoppingCart, {
+            props: {
+                isVisible: false,
+                objAdded: [],
+            }
+        });
+        expect(wrapper.isVisible()).toBe(false);
+    });
+
+    it('Emits toggleCart event when icon is clicked', async () => {
+        const wrapper = shallowMount(ShoppingCart, {
+            props: {
+                isVisible: true,
+                objAdded: [],
+            }
         });
 
-        expect(WRAPPER.text()).toMatch(ITEMS);
-    })
-})
+        await wrapper.find('.icon-link').trigger('click');
+        expect(wrapper.emitted('toggleCart')).toBeTruthy();
+    });
+
+    // Agrega más pruebas según tus necesidades
+});
